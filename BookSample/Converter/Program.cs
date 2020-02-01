9a08623f15e8a81ae3ff030b1fb7600d6bc38a77
blagimi <blagimi@outlook.com>
2020-02-01 19:10:47 +0500

    3

---
+++ BookSample/Converter/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+    public class UnitConverter //Обьявление класса
+{
+    int ratio;                                                  //Поле
+    public UnitConverter(int unitRatio) { ratio = unitRatio; }  //Конструктор
+    public int Convert (int unit) { return unit * ratio; }      //Обьявление метода с входящим значением unit, умножение значения unit на передаваемое в поле ratio значение, возвращение результата
+}                              //Конец класса
+
+    class Program   //Обьявление класса
+    {
+        static void Main() //Обьявление метода
+        {
+        UnitConverter feetToInchesConverter = new UnitConverter(12);    
+        UnitConverter milesToFeetConverter = new UnitConverter(5280);
+        Console.WriteLine(feetToInchesConverter.Convert(30));                               //результат 360
+        Console.WriteLine(feetToInchesConverter.Convert(100));                              //Результат 1200
+        Console.WriteLine(feetToInchesConverter.Convert(milesToFeetConverter.Convert(1)));  //Результат 63360
+        }                 //Конец метода
+    }              //Конец класса
+
